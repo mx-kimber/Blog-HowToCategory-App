2a1b18f1254c9ca3122e6111c94aa0c902c967c8
Kimber Ogden <mx.kimber@gmail.com>
2023-04-07 23:39:19 -0500

    add new column 'category_id' + pass as parameter in 'blogs' controller / migrate - schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_035325) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_042510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_035325) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
